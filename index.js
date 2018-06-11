4b76b7120af419d8c2b0b88aa239d261b08a28da
abely <abdelkader.elyazidi@smile-maroc.com>
2018-06-11 13:56:37 +0000

    Update Index.js

--- index.js
+++ index.js
@@ -96,40 +96,22 @@ function sendButtonMessage(sender, text) {
 }
 
 function sendButtonBuy(sender, text) {
-let messageData = {
-	"attachment":{
-		"type":"template",
-		"payload":{
-			"template_type":"generic",
-			"elements":[
-				 {
-					"title":text,
-					"image_url":"https://petersfancybrownhats.com/company_image.png",
-					"subtitle":"We have the right hat for everyone.",
-					"default_action": {
-						"type": "web_url",
-						"url": "https://petersfancybrownhats.com/view?item=103",
-						"messenger_extensions": false,
-						"webview_height_ratio": "tall",
-						"fallback_url": "https://petersfancybrownhats.com/"
-					},
-					"buttons":[
-						{
-							"type":"web_url",
-							"url":"https://petersfancybrownhats.com",
-							"title":"View Website"
-						},{
-							"type":"postback",
-							"title":"Start Chatting",
-							"payload":"startchatting"
-						}
-					]
-				}
-			]
-		}
+	let messageData = {
+		"attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+          {
+            "title":"Welcome!",
+            "image_url":"https://scontent-mrs1-1.xx.fbcdn.net/v/t1.0-9/26167488_139883043463219_5905871390861314564_n.jpg?_nc_cat=0&oh=87c6678977d536862c165456990810f7&oe=5BA635D3",
+						"subtitle":"We have the right hat for everyone."
+					}
+        ]
+      }
+    }
 	}
-}
-sendRequest(sender, messageData)
+	sendRequest(sender, messageData)
 }
 
 
