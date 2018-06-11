20fe89ec97ff4091114046c769163dcca667c93f
abely <abdelkader.elyazidi@smile-maroc.com>
2018-06-11 13:02:49 +0000

    Update Index.js

--- index.js
+++ index.js
@@ -100,13 +100,30 @@ let messageData = {
 	"attachment":{
 		"type":"template",
 		"payload":{
-			"template_type":"button",
-			"text":"Try the buy button!",
-			"buttons":[
-				{
-					"type":"payment",
-					"title":"Buy Button",
-					"payload":"buy button",
+			"template_type":"generic",
+			"elements":[
+				 {
+					"title":"Welcome!",
+					"image_url":"https://petersfancybrownhats.com/company_image.png",
+					"subtitle":"We have the right hat for everyone.",
+					"default_action": {
+						"type": "web_url",
+						"url": "https://petersfancybrownhats.com/view?item=103",
+						"messenger_extensions": false,
+						"webview_height_ratio": "tall",
+						"fallback_url": "https://petersfancybrownhats.com/"
+					},
+					"buttons":[
+						{
+							"type":"web_url",
+							"url":"https://petersfancybrownhats.com",
+							"title":"View Website"
+						},{
+							"type":"postback",
+							"title":"Start Chatting",
+							"payload":"startchatting"
+						}
+					]
 				}
 			]
 		}
